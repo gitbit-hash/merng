47c7a0afd117d31b6d81921e33729699f2144be8
unknown <codeprof2016@hotmail.com>
2020-11-06 22:42:51 +0200

    configuring mongoose connection

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 
+const mongoose = require('mongoose');
+const { MONGODB } = require('./config');
+
 const typeDefs = gql`
   type Query{
     sayHi: String!
@@ -18,7 +21,16 @@ const server = new ApolloServer({
   resolvers
 });
 
-server.listen({ port: 5000 })
+mongoose.connect(MONGODB,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('MongoDB connected');
+    return server.listen({ port: 5000 });
+  })
   .then(res => {
     console.log(`Server running at ${res.url}`);
   });
+
