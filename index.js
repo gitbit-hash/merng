c8c8b1620da1e0f9233c8e6bad6b73f537ea9815
unknown <codeprof2016@hotmail.com>
2020-11-07 00:57:40 +0200

    implementing a resolver to fetch a post from db

--- index.js
+++ index.js
@@ -4,15 +4,30 @@ const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 const { MONGODB } = require('./config');
 
+const Post = require('./models/Post');
+
 const typeDefs = gql`
+  type Post {
+      id: ID!
+      body: String!
+      userName: String!
+      createdAt: String!
+  }
   type Query{
-    sayHi: String!
+    getPosts: [Post]
   }
 `
 
 const resolvers = {
   Query: {
-    sayHi: () => 'Hello There'
+    async getPosts() {
+      try {
+        const posts = Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
   }
 }
 
