bdc71ed88dc1ef4016b85a5038be1de75d6368fa
unknown <codeprof2016@hotmail.com>
2020-11-12 01:16:39 +0200

    adding react router and setup basic routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 const App = () => (
-  <div>hi world!!!!</div>
+  <Router>
+    <Route exact path='/' component={HomePage} />
+    <Route exact path='/login' component={LoginPage} />
+    <Route exact path='/register' component={RegisterPage} />
+    <Route />
+  </Router>
 );
 
 export default App;
