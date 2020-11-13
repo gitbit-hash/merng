b805c6954a83f669a1c500d8ef12f78b93436100
unknown <codeprof2016@hotmail.com>
2020-11-13 23:47:32 +0200

    adding menu-bar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 
-import 'semantic-ui-css/semantic.min.css';
-import './App.css';
+import MenuBar from './components/MenuBar'
+
+import { Container } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
+import './App.css'
 
 const App = () => (
   <Router>
-    <Route exact path='/' component={HomePage} />
-    <Route exact path='/login' component={LoginPage} />
-    <Route exact path='/register' component={RegisterPage} />
-    <Route />
+    <Container>
+      <MenuBar />
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/login' component={LoginPage} />
+      <Route exact path='/register' component={RegisterPage} />
+      <Route />
+    </Container>
   </Router>
 );
 
-export default App;
+export default App
